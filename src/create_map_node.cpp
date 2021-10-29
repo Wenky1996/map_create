68910e98bfd7706f6618d86a16ce6fce2660b6ed
zwk_ubuntu2020 <wenkyjong1996@gmail.com>
2021-10-29 20:08:23 +0800

    10.26

--- src/create_map_node.cpp
+++ src/create_map_node.cpp
@@ -12,20 +12,27 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <nav_msgs/Odometry.h>
 #include <visualization_msgs/MarkerArray.h>
+
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/common/transforms.h>
 
-#include <message_filters/subscriber.h>
+#include <pcl_conversions/pcl_conversions.h>
 
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr map;
-
+pcl::PointCloud<pcl::PointXYZRGB>::Ptr map(new pcl::PointCloud<pcl::PointXYZRGB>());
 
 std::queue<nav_msgs::OdometryPtr> pose_vio_buf;
 std::queue<sensor_msgs::PointCloud2ConstPtr> pointcloud_buf;
 
 void PointCloudHandler(const sensor_msgs::PointCloud2ConstPtr &pointCloudmsg);
-void KeyPoseHandler(const nav_msgs::Odometry::ConstPtr &vioKeyPose);
+void KeyPoseHandler(const visualization_msgs::MarkerArray::ConstPtr &vioKeyPose);
 void CreatMap();
+
+
+Eigen::Isometry3d NavMsg2Isometry3D(nav_msgs::Odometry::Ptr NavMsg);
+
+ros::Publisher map_pub;
+
 
 int main(int argc ,char **argv){
     ros::init(argc, argv, "map_create");
@@ -33,13 +40,16 @@ int main(int argc ,char **argv){
     ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
     ros::Subscriber subPointCloud = n.subscribe<sensor_msgs::PointCloud2>("/fusion_pointcloud", 100, PointCloudHandler);
     ros::Subscriber subkeyPose = n.subscribe("key_odometrys",2000,KeyPoseHandler);
+    map_pub = n.advertise<sensor_msgs::PointCloud2>("create_map", 100);
     std::thread creat_map_thread{CreatMap};
     ros::spin();
     return 0;
 }
 
 void PointCloudHandler(const sensor_msgs::PointCloud2ConstPtr &pointCloudmsg) {
+
     pointcloud_buf.push(pointCloudmsg);
+
 }
 
 void KeyPoseHandler(const visualization_msgs::MarkerArray::ConstPtr &vioKeyPose){
@@ -53,26 +63,36 @@ void KeyPoseHandler(const visualization_msgs::MarkerArray::ConstPtr &vioKeyPose)
     vioOdometry->pose.pose.orientation.w=vioKeyPose->markers.front().pose.orientation.w;
     vioOdometry->header.stamp=vioKeyPose->markers.front().header.stamp;
     pose_vio_buf.push(vioOdometry);
-
 }
 
 void CreatMap(){
     nav_msgs::Odometry::Ptr vioPose;
     while(true){
-        sensor_msgs::PointCloud2ConstPtr pointCloud;
+        sensor_msgs::PointCloud2ConstPtr pointCloudMsg;
         vioPose=pose_vio_buf.front();
         auto vio_stamp=vioPose->header.stamp.toSec();
-        pointCloud = pointcloud_buf.front();
-        auto pointCloud_stamp = pointCloud->header.stamp.toSec();
-        if(abs(vio_stamp-pointCloud_stamp)<0.03){//1/30小于1帧
+        pointCloudMsg = pointcloud_buf.front();
+        int i_cnt=0;
+        auto pointCloud_stamp = pointCloudMsg->header.stamp;
+        if(abs(vio_stamp-pointCloud_stamp.toSec())<0.03){//1/30小于1帧
             pose_vio_buf.pop();
             pointcloud_buf.pop();
+            pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointCloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+            pcl::fromROSMsg(*pointCloudMsg,*pointCloud);
             Eigen::Isometry3d transform_pose = Eigen::Isometry3d::Identity();
-            transform_pose.translate(vioPose->pose.pose.position);
-
-            
+            transform_pose = NavMsg2Isometry3D(vioPose);
+            pcl::PointCloud<pcl::PointXYZRGB>::Ptr transformed_point(new pcl::PointCloud<pcl::PointXYZRGB>());
+            pcl::transformPointCloud(*pointCloud,*transformed_point,transform_pose.cast<float>());
+            *map+=*transformed_point;
+            if(i_cnt%30==0){
+                sensor_msgs::PointCloud2 pointMapMsg;
+                pcl::toROSMsg(*map,pointMapMsg);
+                pointMapMsg.header.stamp=pointCloud_stamp;
+                pointMapMsg.header.frame_id="map";
+                map_pub.publish(pointMapMsg);
+            }
         } else{
-            if(vio_stamp>pointCloud_stamp){
+            if(vio_stamp>pointCloud_stamp.toSec()){
                 pointcloud_buf.pop();
             } else{
                 pose_vio_buf.pop();
@@ -82,9 +102,20 @@ void CreatMap(){
         std::this_thread::sleep_for(dura);
     }
 }
-
-Eigen::Isometry3d NavMsg2Isometry3D(sensor_msgs::PointCloud2ConstPtr NavMsg){
+/*
+ *    transform NavMsg to Eigen Isometry3d
+ */
+Eigen::Isometry3d NavMsg2Isometry3D(nav_msgs::Odometry::Ptr NavMsg){
     Eigen::Quaterniond pose_quaternion(1,0,0,0);
-    Eigen::Vector3d pose_transalte
-
+    Eigen::Vector3d pose_transalte(0,0,0);
+    Eigen::Isometry3d T = Eigen::Isometry3d::Identity();
+    pose_quaternion.w()=NavMsg->pose.pose.orientation.w;
+    pose_quaternion.x()=NavMsg->pose.pose.orientation.x;
+    pose_quaternion.y()=NavMsg->pose.pose.orientation.y;
+    pose_quaternion.z()=NavMsg->pose.pose.orientation.z;
+    pose_transalte<<NavMsg->pose.pose.position.x,NavMsg->pose.pose.position.y,NavMsg->pose.pose.position.z;
+    T.rotate(pose_quaternion);
+    T.pretranslate(pose_transalte);//Applies on the left the translation matri
+                                   // x represented by the vector other to *this and returns a reference to
+    return  T;
 }
