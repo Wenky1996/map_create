0ae25884b6260cc8adaf4070450536e457d55dd5
wenky <wenkyjong1996@gmail.com>
2021-11-17 16:26:29 +0800

    11.17

--- src/create_map_node.cpp
+++ src/create_map_node.cpp
@@ -8,28 +8,28 @@
 #include <queue>
 #include <thread>
 #include <ros/ros.h>
+
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <nav_msgs/Odometry.h>
-#include <visualization_msgs/MarkerArray.h>
 
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/common/transforms.h>
+#include <pcl-1.8/pcl/point_cloud.h>
+#include <pcl-1.8/pcl/point_types.h>
+#include <pcl-1.8/pcl/common/transforms.h>
 
 #include <pcl_conversions/pcl_conversions.h>
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr map(new pcl::PointCloud<pcl::PointXYZRGB>());
 
-std::queue<nav_msgs::OdometryPtr> pose_vio_buf;
+std::queue<nav_msgs::Odometry::ConstPtr> pose_vio_buf;
 std::queue<sensor_msgs::PointCloud2ConstPtr> pointcloud_buf;
 
 void PointCloudHandler(const sensor_msgs::PointCloud2ConstPtr &pointCloudmsg);
-void KeyPoseHandler(const visualization_msgs::MarkerArray::ConstPtr &vioKeyPose);
+void KeyPoseHandler(const nav_msgs::Odometry::ConstPtr &vioKeyPose);
 void CreatMap();
 
 
-Eigen::Isometry3d NavMsg2Isometry3D(nav_msgs::Odometry::Ptr NavMsg);
+Eigen::Isometry3d NavMsg2Isometry3D(nav_msgs::Odometry::ConstPtr NavMsg);
 
 ros::Publisher map_pub;
 
@@ -39,7 +39,7 @@ int main(int argc ,char **argv){
     ros::NodeHandle n;
     ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
     ros::Subscriber subPointCloud = n.subscribe<sensor_msgs::PointCloud2>("/fusion_pointcloud", 100, PointCloudHandler);
-    ros::Subscriber subkeyPose = n.subscribe("key_odometrys",2000,KeyPoseHandler);
+    ros::Subscriber subkeyPose = n.subscribe("/pose_graph/camera_pose",2000,KeyPoseHandler);
     map_pub = n.advertise<sensor_msgs::PointCloud2>("create_map", 100);
     std::thread creat_map_thread{CreatMap};
     ros::spin();
@@ -47,26 +47,15 @@ int main(int argc ,char **argv){
 }
 
 void PointCloudHandler(const sensor_msgs::PointCloud2ConstPtr &pointCloudmsg) {
-
     pointcloud_buf.push(pointCloudmsg);
-
 }
 
-void KeyPoseHandler(const visualization_msgs::MarkerArray::ConstPtr &vioKeyPose){
-    nav_msgs::Odometry::Ptr vioOdometry;
-    vioOdometry->pose.pose.position.x = vioKeyPose->markers.front().pose.position.x;
-    vioOdometry->pose.pose.position.y = vioKeyPose->markers.front().pose.position.y;
-    vioOdometry->pose.pose.position.z = vioKeyPose->markers.front().pose.position.z;
-    vioOdometry->pose.pose.orientation.z = vioKeyPose->markers.front().pose.orientation.z;
-    vioOdometry->pose.pose.orientation.y=vioKeyPose->markers.front().pose.orientation.y;
-    vioOdometry->pose.pose.orientation.x=vioKeyPose->markers.front().pose.orientation.x;
-    vioOdometry->pose.pose.orientation.w=vioKeyPose->markers.front().pose.orientation.w;
-    vioOdometry->header.stamp=vioKeyPose->markers.front().header.stamp;
-    pose_vio_buf.push(vioOdometry);
+void KeyPoseHandler(const nav_msgs::Odometry::ConstPtr &vioKeyPose){
+    pose_vio_buf.push(vioKeyPose);
 }
 
 void CreatMap(){
-    nav_msgs::Odometry::Ptr vioPose;
+    nav_msgs::Odometry::ConstPtr vioPose;
     while(true){
         sensor_msgs::PointCloud2ConstPtr pointCloudMsg;
         vioPose=pose_vio_buf.front();
@@ -80,7 +69,7 @@ void CreatMap(){
             pcl::PointCloud<pcl::PointXYZRGB>::Ptr pointCloud(new pcl::PointCloud<pcl::PointXYZRGB>());
             pcl::fromROSMsg(*pointCloudMsg,*pointCloud);
             Eigen::Isometry3d transform_pose = Eigen::Isometry3d::Identity();
-            transform_pose = NavMsg2Isometry3D(vioPose);
+            transform_pose =  NavMsg2Isometry3D(vioPose);
             pcl::PointCloud<pcl::PointXYZRGB>::Ptr transformed_point(new pcl::PointCloud<pcl::PointXYZRGB>());
             pcl::transformPointCloud(*pointCloud,*transformed_point,transform_pose.cast<float>());
             *map+=*transformed_point;
@@ -105,7 +94,7 @@ void CreatMap(){
 /*
  *    transform NavMsg to Eigen Isometry3d
  */
-Eigen::Isometry3d NavMsg2Isometry3D(nav_msgs::Odometry::Ptr NavMsg){
+Eigen::Isometry3d NavMsg2Isometry3D(nav_msgs::Odometry::ConstPtr NavMsg){
     Eigen::Quaterniond pose_quaternion(1,0,0,0);
     Eigen::Vector3d pose_transalte(0,0,0);
     Eigen::Isometry3d T = Eigen::Isometry3d::Identity();
